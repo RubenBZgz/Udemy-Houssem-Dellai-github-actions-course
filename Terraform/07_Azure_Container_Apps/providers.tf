25a894c46b5b30442ae7285db24bb92769c22f1f
rbaquedano <rbaquedano@kpmg.es>
2024-11-26 17:12:33 +0100

    Update providers.tf

--- Terraform/07_Azure_Container_Apps/providers.tf
+++ Terraform/07_Azure_Container_Apps/providers.tf
@@ -1,12 +1,11 @@
-provider "azurerm" {
-  features {}
-}
-
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.6.0"
+  cloud {
+    organization = "$HCP_ORGANIZATION"
+
+    workspaces {
+      name = "$HCP_WORKSPACE"
     }
+    #token = "$TF_TOKEN_app_terraform_io"
+
   }
 }
