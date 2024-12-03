9e649326efbec1fb311b5cfe11059c7ecf15aa04
rbaquedano <rbaquedano@kpmg.es>
2024-12-03 10:55:12 +0100

    Modificación TF Install

--- Terraform/07_Azure_Container_Apps/providers.tf
+++ Terraform/07_Azure_Container_Apps/providers.tf
@@ -1,27 +1,14 @@
-/*provider "azurerm" {
-  features {}
-}
-
-terraform {
-  cloud {
-    organization = "$HCP_ORGANIZATION"
-
-    workspaces {
-      name = "$HCP_WORKSPACE"
-    }
-  }
-}*/
-
-
-provider "azurerm" {
-  features {}
-}
-
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.6.0"
+      version = "4.12.0"
     }
+  }
+}
+
+provider "azurerm" {
+  features {
+
   }
 }
