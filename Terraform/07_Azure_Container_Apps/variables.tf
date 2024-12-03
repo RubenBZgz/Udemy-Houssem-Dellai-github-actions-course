9d033e6824e846d3882e1dbb171c287f41a7da4c
rbaquedano <rbaquedano@kpmg.es>
2024-12-03 18:02:06 +0100

    Terraform plan fix 2

--- Terraform/07_Azure_Container_Apps/variables.tf
+++ Terraform/07_Azure_Container_Apps/variables.tf
@@ -1,14 +1,17 @@
 variable "subscription_id" {
   description = "The Azure subscription ID"
   type        = string
+  default     = ""
 }
 
 variable "HCP_ORGANIZATION" {
   type        = string
   description = "HCP Terraform organization"
+  default     = ""
 }
 
 variable "HCP_WORKSPACE" {
   type        = string
   description = "HCP Terraform workspace"
+  default     = ""
 }
